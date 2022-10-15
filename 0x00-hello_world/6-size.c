5f2ba99ff81f9a26ee3989d8a3407e0d98c37845
Ifeanyit103 <ifeanyit103@gmail.com>
2022-10-15 10:30:19 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("size of an int: %lu 4 byte(s)\n", sizeof(int));
         printf("size of a long int: %lu 8 byte(s)\n", sizeof(long int));
         printf("size of a long long int: %lu 8 byte(s)\n", sizeof(long long int));
-	printf("size of a float: %lu byte(s)\n", sizeof(float));
+	printf("size of a float: %lu 4 byte(s)\n", sizeof(float));
         return (0);
 }
 
