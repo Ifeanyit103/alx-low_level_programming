a923bd7107623b056f32adf71c21ed23ac624a45
Ifeanyit103 <ifeanyit103@gmail.com>
2022-10-21 08:15:36 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 	int num, a;
 
-	for (a = 1; a <= 10; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (num = 0; num <= 9; num++)
+		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
 			{
