c6033c41ba39fac7f31f7bdd5f65e002bf6e663c
Ifeanyit103 <ifeanyit103@gmail.com>
2022-10-21 17:40:29 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,9 @@ int main(void)
 	int k;
 
 	printf("1");
-	for (k = 2; k <= 100; k++)
+	for (k = 1; k <= 100; k++)
 	{
-		printf(' ');
+		printf(" ");
 		if (k % 3 == 0)
 			printf("fizz");
 		if (k % 5 == 0)
